114e2087bcd9921eb655fef5e298077f1c70f91e
Fikri Rizal H <fikri.rizal@widyatama.ac.id>
2026-08-26 20:18:05 +0700

    fix: resolve login conflict and apply admin template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,6 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-<<<<<<< Updated upstream
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="utf-8" />
     <title>Log In Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,7 +16,6 @@
 
     <!-- Icons -->
     <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-
 </head>
 
 <body class="bg-white">
@@ -46,27 +34,22 @@
                                 </div>
 
                                 <div class="pt-0">
-                                    <!--  -->
                                     <form method="POST" action="{{ route('admin.login') }}" class="my-4">
                                         @csrf
 
-                                        <!--  -->
                                         @if (session('error'))
                                         <div class="alert alert-danger">
                                             {{ session('error') }}
                                         </div>
                                         @endif
-                                        <!--  -->
 
                                         <div class="form-group mb-3">
-
                                             <label for="emailaddress" class="form-label">Email address</label>
                                             <input class="form-control" type="email" id="email" name="email" required="" placeholder="Enter your email">
 
                                             @error('email')
                                             <small class="text-danger">{{ $message }}</small>
                                             @enderror
-
                                         </div>
 
                                         <div class="form-group mb-3">
@@ -76,11 +59,9 @@
                                             @error('password')
                                             <small class="text-danger">{{ $message }}</small>
                                             @enderror
-
                                         </div>
 
                                         <div class="form-group d-flex mb-3">
-
                                         </div>
 
                                         <div class="form-group mb-0 row">
@@ -129,7 +110,6 @@
                                             </a>
                                         </div>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
@@ -147,39 +127,7 @@
                     </div>
                 </div>
             </div>
->>>>>>> Stashed changes
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</body>
+</html>
