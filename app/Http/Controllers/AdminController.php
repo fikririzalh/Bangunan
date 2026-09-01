2b347de651c91ce6c450c27572794bb73f911943
Fikri Rizal H <fikri.rizal@widyatama.ac.id>
2026-09-01 20:40:55 +0700

    feat: add alert notification and sync header profile avatar

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -109,7 +109,12 @@ class AdminController extends Controller
 
         $data->save();
 
-        return redirect()->back();
+        $notification = array(
+            'message'=>'Profile Updated Successfully',
+            'alert-type'=> 'success'
+        );
+
+        return redirect()->back()->with($notification);
         
     }
     
